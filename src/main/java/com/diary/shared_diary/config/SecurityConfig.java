ac5e37e4a3a9776e829af8ec9638e04fcedb88ff
callmejeje <choije401@naver.com>
2025-07-04 16:12:15 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/com/diary/shared_diary/config/SecurityConfig.java
+++ src/main/java/com/diary/shared_diary/config/SecurityConfig.java
@@ -5,13 +5,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.Map;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
