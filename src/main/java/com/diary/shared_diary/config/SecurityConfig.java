3b2ce23509e71ddf20331dc5008d4a6503a449b5
callmejeje <choije401@naver.com>
2026-01-21 18:22:31 +0900

    chore: API 문서화를 위해 Swagger(OpenAPI) 추가
    `springdoc-openapi-starter-webmvc-ui` 의존성을 추가하여 Swagger UI를 프로젝트에 통합했습니다.
    
    `SecurityConfig`를 수정하여 `/v3/api-docs` 및 `/swagger-ui` 관련 경로에 대한 익명 접근을 허용했습니다.

--- src/main/java/com/diary/shared_diary/config/SecurityConfig.java
+++ src/main/java/com/diary/shared_diary/config/SecurityConfig.java
@@ -33,7 +33,9 @@ public class SecurityConfig {
                         .disable()
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login/**", "/oauth2/**", "/login/oauth2/**", "/h2-console/**", "/dev/**", "/api/auth/refresh", "/api/auth/token").permitAll()
+                        .requestMatchers("/login/**", "/oauth2/**", "/login/oauth2/**", "/h2-console/**", "/dev/**", "/api/auth/refresh", "/api/auth/token", "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/groups/**", "/api/diaries/**", "/api/users/me", "/api/users/public-key").authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
